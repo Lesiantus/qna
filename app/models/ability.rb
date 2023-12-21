283a71722cc0cfa24d75192221ca17573695e193
Lesiantus <sashkabonan@gmail.com>
2023-12-21 20:26:39 +0700

    changed abilities back

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -25,9 +25,7 @@ class Ability
   def user_abilities
     guest_abilities
     can :create, [Question, Answer, Comment]
-    can :update, [Question, Answer] do |updatable|
-      user.author?(updatable)
-    end
+    can :update, [Question, Answer], user_id: user.id
     can :destroy, [Question, Answer], user_id: user.id
     can :add_comment, [Question, Answer]
 
