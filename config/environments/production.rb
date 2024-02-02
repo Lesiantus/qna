ae6ae420a5bbf5307aea0cdd7196ff02dc5e5ff4
Lesiantus <sashkabonan@gmail.com>
2024-02-02 19:55:31 +0700

    added mailer credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,8 +69,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    user_name:            ENV.fetch('SMTP_USERNAME'),
-    password:             ENV.fetch('SMTP_PASSWORD'),
+    user_name:            Rails.application.credentials.gmail[:username],
+    password:             Rails.application.credentials.gmail[:password],
     authentication:       'plain',
     enable_starttls_auto: true
   }
